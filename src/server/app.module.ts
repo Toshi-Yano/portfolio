c77bd29c171aabdec837ac560b289b59fe5eac62
Toshi-Yano <t.yano.0726.6119@gmail.com>
2022-12-02 03:22:18 +0900

    [change]nextによるレンダリング設定完了

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { RenderModule } from 'nest-next';
+import next from 'next';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
   controllers: [AppController],
+  imports: [RenderModule.forRootAsync(next({ dev: true }), { viewsDir: null })],
   providers: [AppService],
 })
 export class AppModule {}
