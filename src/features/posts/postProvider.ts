2bb742e57d99dfc02d5276dafe28f87446703cc0
Toshi-Yano <t.yano.0726.6119@gmail.com>
2023-01-05 03:57:15 +0900

    [change]OGP画像を表示用レコードに割り当て

--- src/features/posts/postProvider.ts
+++ src/features/posts/postProvider.ts
@@ -1,5 +1,15 @@
+import { generateOGP } from './ogpGenerator';
+
 import fetchQiitaPosts from '@/features/posts/qiita';
 
 export default async function fetchPosts() {
-  return await fetchQiitaPosts();
+  const posts = await fetchQiitaPosts();
+
+  return await Promise.all(
+    posts.map(async (post) => {
+      const ogpUrl = await generateOGP(post.url);
+      post['ogpUrl'] = ogpUrl;
+      return post;
+    }),
+  );
 }
