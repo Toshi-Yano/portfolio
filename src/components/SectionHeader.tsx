45d9fca8f62da478f5806f706f99dc1979abf2d9
Toshi-Yano <t.yano.0726.6119@gmail.com>
2023-01-10 15:35:51 +0900

    [change]レイアウト微調整

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function SectionHeader({ children, title }: Props) {
   return (
     <>
-      <h2 className='mx-auto mt-4 mb-5 border-b border-gray-500 pb-2 text-center text-2xl font-bold tracking-wider sm:text-3xl'>
+      <h2 className='mx-auto mt-4 mb-5 border-b border-gray-500 pb-2 text-center text-2xl font-bold tracking-wider'>
         {title}
       </h2>
       {children}
