3c1f413c4c3d36a7fbbaf37eed5a80b15aa12939
Toshi-Yano <t.yano.0726.6119@gmail.com>
2023-01-11 23:08:59 +0900

    [change]フラグメント→sectionで囲んだセクションを返却するよう修正

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -5,11 +5,11 @@ type Props = {
 
 export default function SectionHeader({ children, title }: Props) {
   return (
-    <>
+    <section>
       <h2 className='mx-auto mt-4 mb-5 border-b border-gray-500 pb-2 text-center text-2xl font-bold tracking-wider'>
         {title}
       </h2>
       {children}
-    </>
+    </section>
   );
 }
